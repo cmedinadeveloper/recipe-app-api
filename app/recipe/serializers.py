8826d63d92cfd38a4102887090af73e9c01f9a5e
Carlos Medina <cmedina.developer@gmail.com>
2023-02-16 21:44:20 -0400

    Update recipe serializer to handle Tags creating and updates

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -17,7 +17,7 @@ class TagSerializer(serializers.ModelSerializer):
 
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipe objects"""
-    tags = TagSerializer(many=True, read_only=False)
+    tags = TagSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
@@ -25,9 +25,8 @@ class RecipeSerializer(serializers.ModelSerializer):
                   'price', 'description', 'link', 'tags']
         read_only_fields = ['id']
 
-    def create(self, validated_data):
-        tags = validated_data.pop('tags', [])
-        recipe = Recipe.objects.create(**validated_data)
+    def _get_or_create_tags(self, tags, recipe):
+        """Handle getting or creating tags as needed"""
         auth_user = self.context['request'].user
         for tag in tags:
             tag_obj, _ = Tag.objects.get_or_create(
@@ -35,6 +34,26 @@ class RecipeSerializer(serializers.ModelSerializer):
                 **tag
             )
             recipe.tags.add(tag_obj)
+
+    def create(self, validated_data):
+        tags = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+        self._get_or_create_tags(tags, recipe)
+
+        return recipe
+
+    def update(self, instance, validated_data):
+        """Update recipe"""
+        tags = validated_data.pop('tags', None)
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
 
 
 class RecipeDetailSerializer(RecipeSerializer):
