de3a71cf5ba2a4ba5628b83e7252e5d2f724a8ca
Carlos Medina <cmedina.developer@gmail.com>
2025-02-04 15:01:05 -0400

    [fix]: final linting fix on checks.yml

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t&_7^h$vh20nv3%e^o+(291@@q91#&=!qo=+eu&#j&*s6es!6c'
+SECRET_KEY = (
+    'django-insecure-t&_7^h$vh20nv3%e^o+(291@@q91#&=!qo=+eu&#j&*s6es!6c'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
